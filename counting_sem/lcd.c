f6714967d4c8641fd87a46971df8d1f447536197
Akshay P Kumar <93068695+akshayembedded@users.noreply.github.com>
2023-08-13 11:48:35 +0530

    updated counting semaphore

--- counting_sem/lcd.c
+++ counting_sem/lcd.c
@@ -8,10 +8,11 @@ void lcd2(void *a);
 void lcd3(void *a);
 void display(const char *);
 void trans(char a);
+
+int a[5]={8,3,5,6,7},index=0;
+
 int main()
 {
-	m=xSemaphoreCreateCounting(5,5);
-	m2=xSemaphoreCreateMutex();
 	PINSEL0=1|1<<2;
 	U0LCR=0x83;
 	U0DLL=98;
@@ -21,6 +22,18 @@ int main()
 	PINSEL1=0;
 	IO0DIR=0;
 	IO1DIR=~0;
+	m=xSemaphoreCreateCounting(5,5);
+	if(m==NULL)
+		display("Failed To Create Counting Semaphore\r\n");
+	else
+		display("Successfully Created Counting Semaphore\r\n");
+	
+	m2=xSemaphoreCreateMutex();
+	if(m2==NULL)
+		display("Failed To Create Mutex\r\n");
+	else
+		display("Successfully Created Mutex\r\n");
+	
 	xTaskCreate(lcd1,"Welcome",90,NULL,0,NULL);
 	xTaskCreate(lcd2,"hello",90,NULL,0,NULL);
 	xTaskCreate(lcd3,"3rd",90,NULL,0,NULL);
@@ -46,24 +59,28 @@ void lcd1(void *a)
 	{
 		if((IO0PIN&(1<<12))==(1<<12))
 		{	
-		b=uxSemaphoreGetCount(m);
-		if(xSemaphoreTake(m,1000)==1)
-		{			
-			if(xSemaphoreTake(m2,3000)==1)
-			{
-			display("\rIn Take");
-
-			display("\rBefore Taken - ");
-			trans(b+48);
 			b=uxSemaphoreGetCount(m);
-			display("\rAfter Taken - ");
-			trans(b+48);
-				xSemaphoreGive(m2);
-		}
-		while((IO0PIN&(1<<12))==(1<<12));
+			if(xSemaphoreTake(m,1000)==1)
+			{			
+				if(xSemaphoreTake(m2,3000)==1)
+				{
+	
+					display("\r\nArray Value is ");
+					trans(a[index++]+48);
+					if(index==5) 
+						index=0
+					display("\rIn Take");
+					display("\rBefore Taken - ");
+					trans(b+48);
+					b=uxSemaphoreGetCount(m);
+					display("\rAfter Taken - ");
+					trans(b+48);
+					xSemaphoreGive(m2);
+				}
+			while((IO0PIN&(1<<12))==(1<<12));
 		
-	}
-	}
+			}
+		}
 	}
 }
 void lcd3(void *a)
@@ -71,52 +88,53 @@ void lcd3(void *a)
 	char b;
 	while(1)
 	{
-		if((IO0PIN&(1<<14))==(1<<14))
+		if((IO0PIN&(1<<13))==(1<<13))
 		{
 			b=uxSemaphoreGetCount(m);
-				if(xSemaphoreTake(m,1000)==1)
-		{
-			if(xSemaphoreTake(m2,3000)==1)
-			{
-			display("\rIn Take");
-		
+			if(xSemaphoreTake(m,1000)==1)
+			{			
+				if(xSemaphoreTake(m2,3000)==1)
+				{
 	
-			display("\rBefore Taken - ");
-			trans(b+48);
-			b=uxSemaphoreGetCount(m);
-			display("\rAfter Taken - ");
-			trans(b+48);
-				xSemaphoreGive(m2);
-		}
-		while((IO0PIN&(1<<14))==(1<<14));
+					display("\r\nArray Value is ");
+					trans(a[index++]+48);
+					if(index==5) 
+						index=0
+					display("\rIn Take");
+					display("\rBefore Taken - ");
+					trans(b+48);
+					b=uxSemaphoreGetCount(m);
+					display("\rAfter Taken - ");
+					trans(b+48);
+					xSemaphoreGive(m2);
+				}
+			}
+		while((IO0PIN&(1<<13))==(1<<13));
 		
-	}
-	}
+		}
 	}
 }
 void lcd2(void *a)
 {
-int b;
+	char b;
 	while(1)
 	{
-		if((IO0PIN&(1<<13))==(1<<13))
+		if((IO0PIN&(1<<14))==(1<<14))
 		{
 			if(xSemaphoreTake(m2,3000)==1)
 			{
-			b=uxSemaphoreGetCount(m);
-			display("\rIn give");
-						display("\rBefore Given - ");
-			trans(b+48);
-		
-		
-		xSemaphoreGive(m);
-			b=uxSemaphoreGetCount(m);
-			display("\rAfter Given - ");
-			trans(b+48);
-			while((IO0PIN&(1<<13))==(1<<13));
-			xSemaphoreGive(m2);
+				b=uxSemaphoreGetCount(m);
+				display("\rIn give");
+				display("\rBefore Given - ");
+				trans(b+48);
+				xSemaphoreGive(m);
+				b=uxSemaphoreGetCount(m);
+				display("\rAfter Given - ");
+				trans(b+48);
+				while((IO0PIN&(1<<14))==(1<<14));
+				xSemaphoreGive(m2);
+			}
 		}
-	}
 		
 	}
-	}
+}
